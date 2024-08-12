fdd63ca83adff6544a167ccef11a59533adefbe7
santirufiner <santirufiner1@gmail.com>
2024-08-12 20:20:13 -0300

    Adding SQL features Ch.4.2, 4.3 and 4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,15 +16,28 @@ type application struct {
 }
 
 func main() {
+
 	/* Define a new command-line flag with the name 'addr', a default value of ":4000"
 	Esto lo q hace es definir el port via linea de comandos para no tener que hardcodearlo a mano,
 	en caso de no usar la bandera se iniciará automáticamente en el port :4000*/
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+
+	/* Define otra command-line para iniciar la db*/
+	dsn := flag.String("dsn", "web:localhost123@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	// Logger de info y error
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed before the main() function exits.
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -36,6 +52,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr) // Information message
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err) // Error message
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
