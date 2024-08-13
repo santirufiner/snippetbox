474f5923c0d4b5d38ddec0afab5069839136bb5f
santirufiner <santirufiner1@gmail.com>
2024-08-13 17:50:00 -0300

    Adding features Ch.4.7,4.8 and 4.9: single, multiple-records and Transactions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/santirufiner/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -15,24 +17,39 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// archivos html que le voy a pasar a la biblioteca template para que los renderice
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/pages/home.html",
-		"./ui/html/partials/nav.html",
-	}
+	snippets, err := app.snippets.Latest()
 
-	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
-		return
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	/*
+
+		// archivos html que le voy a pasar a la biblioteca template para que los renderice
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/pages/home.html",
+			"./ui/html/partials/nav.html",
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		err = ts.ExecuteTemplate(w, "base", nil)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+	*/
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +58,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -41,10 +42,60 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 // func que retorna un snippet en base al id en la db
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+	// statement query para el insert en la tabla snippets
+	stmt := `SELECT * FROM snippets 
+				WHERE expires > UTC_TIMESTAMP() AND ID = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	// Creo una estructura de snippet vacía y la comparo con la row que recibí,
+	// ya que el método QueryRow() no devuelve un error en sí
+	s := &Snippet{}
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
+
 }
 
 // func que retorna los 10 ultimos snippets de la db
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+
+	stmt := `SELECT * FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// --------------------------------- CRITICO --------------------------------- //
+	// defer siempre después de chequear el error q devuelve el query (Y SIEMPRE VA!)
+	defer rows.Close()
+
+	// Creo un slice para manejar los snippets
+	snippets := []*Snippet{}
+
+	// los rows funcionan como una lista (con iteradores)
+	for rows.Next() {
+		s := &Snippet{}
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
